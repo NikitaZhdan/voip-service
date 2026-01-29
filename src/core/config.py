d03d36a2d4e749868e7c8b24795ba24ca16a43bd
Nikita <zhdan.019@gmail.com>
2026-01-29 15:59:23 +0300

    init config

---
+++ src/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+
+class Settings(BaseSettings):
+    POSTGRES_URL: str
+
+    model_config = SettingsConfigDict(
+        env_file=BASE_DIR / ".env",
+    )
+
+
+settings = Settings()
